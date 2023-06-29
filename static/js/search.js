e69667a880fa05c23c2728b07fd59f670c5cc9a8
Daylin Morgan <daylinmorgan@gmail.com>
2023-06-29 10:20:34 -0500

    style: format js

--- static/js/search.js
+++ static/js/search.js
@@ -2,26 +2,26 @@ const params = new URLSearchParams(window.location.search);
 const query = params.get("query");
 
 let miniSearch = new MiniSearch({
-  fields: ['title', 'content','tags'], // fields to index for full-text search
-  storeFields: ['title', 'display'] // fields to return with search results
-})
+  fields: ["title", "content", "tags"], // fields to index for full-text search
+  storeFields: ["title", "display"], // fields to return with search results
+});
 
-miniSearch.addAll(documents)
+miniSearch.addAll(documents);
 
-let results = miniSearch.search(query)
+let results = miniSearch.search(query);
 
 const searchResults = document.getElementById("results");
 if (results.length) {
   let resultHtml = "";
-  resultHtml += "<p>" + results.length + " results</p>"
-  resultHtml += "<ul>"
+  resultHtml += "<p>" + results.length + " results</p>";
+  resultHtml += "<ul>";
   for (const item of results) {
-    resultHtml += '<li><div class="bg-ctp-base rounded-md p-2 my-2 markdown">'
-    resultHtml += item.display
-    resultHtml += '<a href="' + item.id + '">Read more!</a>'
-    resultHtml +="<div></li>"
+    resultHtml += '<li><div class="bg-ctp-base rounded-md p-2 my-2 markdown">';
+    resultHtml += item.display;
+    resultHtml += '<a href="' + item.id + '">Read more!</a>';
+    resultHtml += "<div></li>";
   }
-  resultHtml += '</ul>'
+  resultHtml += "</ul>";
   searchResults.innerHTML = resultHtml;
 } else {
   searchResults.innerHTML = "No results found.";
